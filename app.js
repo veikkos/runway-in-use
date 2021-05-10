1e69fa6cac80325ffc4b1ccb1fc2489a0bc7bc9d
Veikko Soininen <veikko.soininen@gmail.com>
2021-05-10 16:02:21 +0300

    Runway priority calculations

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
 const xhttp = new XMLHttpRequest();
 
+const maxCrossWindComponent = 20;
+const maxTailWindComponent = 5;
+
+const arrPrio = [
+    { id: "15", course: 145 },
+    { id: "22L", course: 220 },
+    { id: "04L", course: 040 },
+    { id: "04R", course: 040 },
+    { id: "22R", course: 220 },
+    { id: "33", course: 325 },
+];
+
+const depPrio = [
+    { id: "22R", course: 220 },
+    { id: "22L", course: 220 },
+    { id: "04R", course: 040 },
+    { id: "33", course: 325 },
+    { id: "04L", course: 040 },
+    { id: "15", course: 145 },
+];
+
+const isRwyAvailable = (dir, rwy, windDir, windSpeed) => {
+    const toRadians = (angle) => angle * (Math.PI / 180);
+
+    const crossWind = Math.abs(windSpeed * Math.sin(toRadians(windDir) - toRadians(rwy.course)));
+
+    if (crossWind >= maxCrossWindComponent) {
+        console.log(`Too high crosswind for ${rwy.id}`);
+        return false;
+    }
+
+    const headWind = windSpeed * Math.cos(toRadians(windDir) - toRadians(rwy.course));
+
+    if (headWind <= -maxTailWindComponent) {
+        console.log(`Too high headwind for ${rwy.id}`);
+        return false;
+    }
+
+    console.log(`Rwy ${rwy.id} ` +
+        `(${rwy.course} deg) selected for ${dir} ` +
+        `(CWC ${crossWind.toFixed(1)}kt / HWC ${headWind.toFixed(1)}kt))`);
+
+    return true;
+}
+
 const rwyParser = metar => {
     const windRegex = /[0-9A-Z]*KT/g;
     const windFull = metar.match(windRegex);
@@ -20,12 +65,10 @@ const rwyParser = metar => {
         const winDir = parseInt(windOnly.substring(0, 3));
         const windSpeed = parseInt(windOnly.substring(3, 5));
 
-        if (winDir > 130 && winDir < 240 && windSpeed < 15) {
-            return ['22R', '15'];
-        } else {
-            return ['?', '?'];
-        }
-        // more rules ...
+        const dep = depPrio.find(item => isRwyAvailable('dep', item, winDir, windSpeed));
+        const arr = arrPrio.find(item => isRwyAvailable('arr', item, winDir, windSpeed));
+
+        return [dep ? dep.id : '?', arr ? arr.id : '?']
     }
 
     return null;
